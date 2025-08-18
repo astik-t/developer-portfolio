088abab32465f42c5b7498a4f61d1f3c4e405ef7
astik-t <astiktripathi28@gmail.com>
2025-08-18 22:06:15 +0530

    Final commit

--- src/data/aboutData.js
+++ src/data/aboutData.js
@@ -1,5 +1,5 @@
 export const aboutData = {
-    title: "Who I am",
+    title: "Who I am ?",
     description1: "My name is Astik Tripathi. I am a Btech CSE Student and tech enthusiast.",
     description2: "Aspiring software engineer and AI enthusiast pursuing BTech in CSE (AIML) at Graphic Era University. I’m passionate about exploring how intelligent systems and modern computing can solve real-world challenges. I’m currently building my foundations in coding, computer networks, and data structures, while working on small projects to apply my learning!",
     image: 2
--- src/data/headerData.js
+++ src/data/headerData.js
@@ -1,9 +1,12 @@
 import resume from '../assets/pdf/resume.pdf'
+import profile from '../assets/profile.png'
+
 
 export const headerData = {
     name: 'Astik Tripathi',
     title: "Aspiring Developer",
     desciption:"“Shadows gathered, not where the sun set, but where hope once lived.” ",
-    image: "https://ibb.co/r2Qz6xvG",
+    image: profile,
+
     resumePdf: resume
 }
