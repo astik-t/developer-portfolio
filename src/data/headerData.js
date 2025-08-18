cd8972f1059e0c3511e19437ff41409c90c5c59e
astik-t <astiktripathi28@gmail.com>
2025-08-18 21:54:03 +0530

    Update headerData.js

--- src/data/headerData.js
+++ src/data/headerData.js
@@ -4,6 +4,6 @@ export const headerData = {
     name: 'Astik Tripathi',
     title: "Aspiring Developer",
     desciption:"“Shadows gathered, not where the sun set, but where hope once lived.” ",
-    image: "https://drive.google.com/file/d/1Qg90kbXpJD-n529zKN3mk-iAzJ9cnzNH/view?usp=sharing",
+    image: "https://ibb.co/r2Qz6xvG",
     resumePdf: resume
 }
